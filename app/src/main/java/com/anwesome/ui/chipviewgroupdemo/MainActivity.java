1e90c599f8f3e9c747daaeb16c526243b3abe615
Anwesh43 <anweshthecool0@gmail.com>
2016-12-18 16:22:55 +0530

    Adding new images in the demo

--- app/src/main/java/com/anwesome/ui/chipviewgroupdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/chipviewgroupdemo/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ChipViewGroup chipViewGroup = new ChipViewGroup(this);
-        chipViewGroup.addChips(new ChipComponent("Title 1", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 2", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 3", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 4", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 5", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 6", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
-        chipViewGroup.addChips(new ChipComponent("Title 7", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 1", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 2", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 3", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 4", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 5", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 6", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
+        chipViewGroup.addChips(new ChipComponent("Title 7", BitmapFactory.decodeResource(getResources(),R.drawable.ca)));
         setContentView(chipViewGroup);
     }
 }
