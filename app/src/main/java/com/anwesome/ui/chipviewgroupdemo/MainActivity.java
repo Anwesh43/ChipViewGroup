1a86599db4045e019216ccb2c4a1b28c3fb9e5d8
Anwesh43 <anweshthecool0@gmail.com>
2016-12-18 14:44:17 +0530

    Adding Demo file

---
+++ app/src/main/java/com/anwesome/ui/chipviewgroupdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.chipviewgroupdemo;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.ui.chipviewgroup.ChipComponent;
+import com.anwesome.ui.chipviewgroup.ChipViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ChipViewGroup chipViewGroup = new ChipViewGroup(this);
+        chipViewGroup.addChips(new ChipComponent("Title 1", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 2", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 3", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 4", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 5", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 6", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        chipViewGroup.addChips(new ChipComponent("Title 7", BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)));
+        setContentView(chipViewGroup);
+    }
+}
